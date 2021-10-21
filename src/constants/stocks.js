7b19d3488f9db1eaa83e188253828e90c53ec68b
Shriraj <shri.93.raj@gmail.com>
2021-10-22 00:32:29 +0530

    added stocks data

---
+++ src/constants/stocks.js
@@ -0,0 +1,38 @@
+const types = {
+	common: 'COMMON',
+	preferred: 'PREFERRED',
+};
+
+export const stocks = [
+	{
+		symbol: 'TEA',
+		type: types.common,
+		lastDividend: 0,
+		parValue: 100,
+	},
+	{
+		symbol: 'POP',
+		type: types.common,
+		lastDividend: 8,
+		parValue: 100,
+	},
+	{
+		symbol: 'ALE',
+		type: types.common,
+		lastDividend: 23,
+		parValue: 60,
+	},
+	{
+		symbol: 'GIN',
+		type: types.preferred,
+		lastDividend: 8,
+		fixedDividend: 2,
+		parValue: 100,
+	},
+	{
+		symbol: 'JOE',
+		type: types.common,
+		lastDividend: 13,
+		parValue: 250,
+	},
+];
