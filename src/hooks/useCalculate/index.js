0137b9ffb49410e35003302bf5679b7d56f1f61f
Shriraj <shri.93.raj@gmail.com>
2021-10-22 16:07:49 +0530

    Remove unused variables

--- src/hooks/useCalculate/index.js
+++ src/hooks/useCalculate/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { calculateDividendYield, calculatePeRatio } from '../../utils/utils';
 
 const useCalculate = () => {
